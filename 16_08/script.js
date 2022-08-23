a89addfac7851420b516724a570dbaa1faa3bcb8
Bevicci <belusovaviktorija@inbox.lv>
2022-08-23 18:03:02 +0300

    Calculator created

---
+++ 16_08/script.js
@@ -0,0 +1,8 @@
+function addValue(field) {
+    document.getElementById('display').innerHTML = document.getElementById('display').innerHTML + field.innerHTML
+}
+
+
+function delResult() {
+    document.getElementById('display').innerHTML = ''
+}
